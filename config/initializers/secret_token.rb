a820299bb08991905af9650b9d543cb386964752
arbitrary_apple <raitaku7280@gmail.com>
2015-01-02 19:42:09 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameMap::Application.config.secret_key_base = '0a9279447e6facb158cf87cda38bf30f2335d972141f3db2cbfdb50aee4b66dd5f98d8ec88c9bc3db76a88bbc7d5e287b4e242f73fcec15f0e9bbc97fa4dc6ea'
